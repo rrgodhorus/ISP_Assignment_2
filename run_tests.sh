f2c1e062322f9eb0f739dcaa03f397048700db60
Rajath Reghunath <rrgodhorus@gmail.com>
2023-10-23 15:55:47 -0400

    attack case 2 done

--- run_tests.sh
+++ run_tests.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+> test_results.txt
+
 counter=0
 for referencemonitor in reference_monitor_*; do
     ((counter++))
-    echo $counter : $referencemonitor under test
-    for testcase in rr4433_attackcase1*; do
-        test_output=$(python repy.py restrictions.default encasementlib.r2py $referencemonitor $testcase 2>&1)
+    echo $counter : $referencemonitor under test | tee -a test_results.txt
+    for testcase in rr4433_attackcase2*; do
+        test_output=$(python repy.py restrictions.default encasementlib.r2py $referencemonitor $testcase 2>&1 | tee -a test_results.txt)
         if [ -n "$test_output" ]; then
             echo "Output detected"
             echo $test_output | tail -n 1
-            # :
+            # echo ""
         else
-            # echo "No output"
-            :
+            echo ""
         fi
     done
 done
