3f35533c8e0ace5a45cd90dd051f59e0dcf7e5d7
Rajath Reghunath <rrgodhorus@gmail.com>
2023-10-24 14:11:35 -0400

    ac 8 done

--- run_tests.sh
+++ run_tests.sh
@@ -6,7 +6,7 @@ counter=0
 for referencemonitor in reference_monitor_*; do
     ((counter++))
     echo $counter : $referencemonitor under test | tee -a test_results.txt
-    for testcase in rr4433_attackcase7*; do
+    for testcase in rr4433_attackcasen*; do
         test_output=$(python repy.py restrictions.default encasementlib.r2py $referencemonitor $testcase 2>&1 | tee -a test_results.txt)
         if [ -n "$test_output" ]; then
             echo "Output detected"
