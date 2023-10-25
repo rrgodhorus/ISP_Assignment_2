234b4e488aac910d3dc79ce0d81cf0f740f04408
Rajath Reghunath <rrgodhorus@gmail.com>
2023-10-24 23:16:44 -0400

    added cleanup script and saved all testcases against me

---
+++ cleanup_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm test*.txt
+rm attack*
+rm empty.txt
+rm example.txt
+rm foo
+rm somethingmalicious.txt
+rm out.txt
--- run_tests.sh
+++ run_tests.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 > test_results.txt
+> testcases_bypassed.txt
 
 counter=0
-for referencemonitor in reference_monitor_*; do
+for referencemonitor in reference_monitor_rr4433*; do
     ((counter++))
     echo $counter : $referencemonitor under test | tee -a test_results.txt
-    for testcase in rr4433_attackcase[1-9]*; do
+    counter2=0
+    for testcase in *_attackcase*; do
+        ((counter2++))
+        echo $counter2 : $testcase attacking $referencemonitor 
         test_output=$(python repy.py restrictions.default encasementlib.r2py $referencemonitor $testcase 2>&1 | tee -a test_results.txt)
         if [ -n "$test_output" ]; then
             echo "Output detected"
             echo $test_output | tail -n 1
-            echo $referencemonitor >> ref_mons_bypassed.txt
-            break
+            # echo $referencemonitor >> ref_mons_bypassed.txt
+            echo $testcase >> testcases_bypassed.txt
         fi
     done
 done
