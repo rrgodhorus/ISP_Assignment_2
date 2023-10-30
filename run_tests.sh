fcaef0c957bfc94745a0bd1b9387ff4187425370
Rajath Reghunath <rrgodhorus@gmail.com>
2023-10-30 15:02:41 -0400

    save send_to_ta

--- run_tests.sh
+++ run_tests.sh
@@ -8,10 +8,10 @@ for referencemonitor in reference_monitor_rr4433*; do
     ((counter++))
     echo $counter : $referencemonitor under test | tee -a test_results.txt
     counter2=0
-    for testcase in mm13171_attackcase*.r2py; do
+    for testcase in *_attackcase*.r2py; do
         ((counter2++))
         echo $counter2 : $testcase attacking $referencemonitor 
-        test_output=$(python repy.py restrictions.default dylink.r2py encasementlib.r2py $referencemonitor $testcase 2>&1 | tee -a test_results.txt)
+        test_output=$(python repy.py restrictions.default encasementlib.r2py $referencemonitor $testcase 2>&1 | tee -a test_results.txt)
         if [ -n "$test_output" ]; then
             echo "Output detected"
             echo $test_output | tail -n 1
